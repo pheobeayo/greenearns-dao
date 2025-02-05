da6c71fc0850a26f214ba1c7d7fc9a39e35837e0
Khemmie-Ray <atokemmy@gmail.com>
2025-02-05 17:34:11 +0100

    fetch transaction data

--- src/main.jsx
+++ src/main.jsx
@@ -4,10 +4,15 @@ import App from './App.jsx'
 import './index.css'
 import { ToastContainer } from "react-toastify";
 import 'react-toastify/dist/ReactToastify.css';
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+     <QueryClientProvider client={queryClient}>
     <ToastContainer />
     <App />
+    </QueryClientProvider>
   </StrictMode>,
 )
